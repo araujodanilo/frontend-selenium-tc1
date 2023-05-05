da4c0ae3eb7dae00fab0412fa38323e5bf27ac7b
Danilo Araujo <x45danilo45x@gmail.com>
2023-05-05 12:51:49 -0300

    feat(Router): adding error page

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 
+//Routes
+import ErrorPage from './error-page'
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <div>Hello world!</div>,
+        errorElement: <ErrorPage />
     },
 ]);
 
