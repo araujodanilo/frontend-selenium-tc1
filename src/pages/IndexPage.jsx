06314d7479b761451721afa919aac28916ca05b8
Caue <cauegastaldi@outlook.com>
2023-05-12 23:08:21 -0300

    feat(page): created index page

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,14 @@
+export default IndexPage = () => {
+    return (
+        <>
+        <div>
+            <h1>Bem vindo ao site</h1>
+
+            <p>Este é um site que possui como finalidade mostrar registros criminais em aberto</p>
+
+            <button>Visualizar esporte</button>
+            <button>Cadastrar esporte</button>
+        </div>
+        </>
+    )
+}
