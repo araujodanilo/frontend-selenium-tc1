470883c3472b1692693f07a354e59494fccf290c
isarnf <isarnf@gmail.com>
2023-05-12 23:03:19 -0300

    feat: added Index.jsx file containing form

---
+++ src/components/forms/Index.jsx
@@ -0,0 +1,18 @@
+const OpenCriminalCase = () => {
+    return (
+        <>
+        <form action="">
+            <label for="suspeito">Principal suspeito: </label>
+            <input type="text"/>
+            <label for="tipoCrime">Tipo de crime: </label>
+            <input type="text"/>
+            <label for="localCrime">Local do crime: </label>
+            <input type="text"/>
+            <label for="data">Data: </label>
+            <input type="text"/>
+        </form>
+        </>
+    )
+}
+
+export default OpenCriminalCase;
