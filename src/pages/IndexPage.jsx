5e4e9eb61699a9349b654f36f8557074ebd40fc8
Danilo Araujo <x45danilo45x@gmail.com>
2023-05-13 00:06:23 -0300

    style: change indent on file IndexPage.jsx

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,14 +1,16 @@
-export default IndexPage = () => {
-    return (
-        <>
-        <div>
-            <h1>Bem vindo ao site</h1>
+const IndexPage = () => {
+	return (
+		<>
+			<div>
+				<h1>Bem vindo ao site</h1>
 
-            <p>Este é um site que possui como finalidade mostrar registros criminais em aberto</p>
+				<p>Este é um site que possui como finalidade mostrar registros criminais em aberto</p>
 
-            <button>Visualizar registros criminais</button>
-            <button>Cadastrar registro criminal</button>
-        </div>
-        </>
-    )
+				<button>Visualizar registros criminais</button>
+				<button>Cadastrar registro criminal</button>
+			</div>
+		</>
+	)
 }
+
+export default IndexPage;
