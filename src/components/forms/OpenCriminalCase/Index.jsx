2c908cf0359788740c646ef9d573b13223d7afd3
isarnf <isarnf@gmail.com>
2023-05-16 20:18:05 -0300

    feat: added submit button to form

--- src/components/forms/OpenCriminalCase/Index.jsx
+++ src/components/forms/OpenCriminalCase/Index.jsx
@@ -4,12 +4,17 @@ const OpenCriminalCase = () => {
         <form action="">
             <label for="suspeito">Principal suspeito: </label>
             <input type="text"/>
+            <br /><br />
             <label for="tipoCrime">Tipo de crime: </label>
             <input type="text"/>
+            <br /><br />
             <label for="localCrime">Local do crime: </label>
             <input type="text"/>
+            <br /><br />
             <label for="data">Data: </label>
             <input type="text"/>
+            <br /><br />
+            <input type="submit" value="Enviar" />
         </form>
         </>
     )
