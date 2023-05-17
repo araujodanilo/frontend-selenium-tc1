b9d2e70f01c39973744f2e169e3864d5bdb2c23b
Danilo Araujo <x45danilo45x@gmail.com>
2023-05-17 13:15:41 -0300

    fix(form): fixing html

--- src/components/forms/OpenCriminalCase/Index.jsx
+++ src/components/forms/OpenCriminalCase/Index.jsx
@@ -1,23 +1,23 @@
 const OpenCriminalCase = () => {
-    return (
-        <>
-        <form action="">
-            <label for="suspeito">Principal suspeito: </label>
-            <input type="text"/>
-            <br /><br />
-            <label for="tipoCrime">Tipo de crime: </label>
-            <input type="text"/>
-            <br /><br />
-            <label for="localCrime">Local do crime: </label>
-            <input type="text"/>
-            <br /><br />
-            <label for="data">Data: </label>
-            <input type="text"/>
-            <br /><br />
-            <input type="submit" value="Enviar" />
-        </form>
-        </>
-    )
+	return (
+		<>
+			<form >
+				<label htmlFor="suspeito">Principal suspeito: </label>
+				<input type="text" />
+				<br /><br />
+				<label htmlFor="tipoCrime">Tipo de crime: </label>
+				<input type="text" />
+				<br /><br />
+				<label htmlFor="localCrime">Local do crime: </label>
+				<input type="text" />
+				<br /><br />
+				<label htmlFor="data">Data: </label>
+				<input type="text" />
+				<br /><br />
+				<input type="submit" value="Enviar" />
+			</form>
+		</>
+	)
 }
 
 export default OpenCriminalCase;
