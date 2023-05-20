b978c0564d58b01eac3b2a585f4b4b45b506da34
Danilo Araujo <x45danilo45x@gmail.com>
2023-05-20 08:32:08 -0300

    feat(api): adding console log to see requests

--- src/api/OpenCriminalCaseApi.js
+++ src/api/OpenCriminalCaseApi.js
@@ -4,17 +4,22 @@ const baseUrl = "http://localhost:3000/api/crimes"
 export const getAllOpenCriminalCase = async (setData) => {
 	await fetch(`${baseUrl}`, { method: "GET" })
 		.then(async response => {
-			console.log("getAllClients status: ", response.status)
+			console.log(response)
 			const data = (await response.json())
 			setData(data)
+		}).catch(err => {
+			console.log(err)
 		})
 }
 
 export const getOneOpenCriminalCase = async (id, setData) => {
 	fetch(`${baseUrl}/${id}`, { method: "GET" })
 		.then(async response => {
+			console.log(response)
 			const data = (await response.json())
 			setData(data)
+		}).catch(err => {
+			console.log(err)
 		})
 }
 
@@ -26,8 +31,9 @@ export const createOpenCriminalCase = async (data) => {
 			headers: { "Content-type": "application/json; charset=UTF-8" }
 		}).then(response => {
 			console.log(response)
-		}
-		)
+		}).catch(err => {
+			console.log(err)
+		})
 }
 
 export const updateOpenCriminalCase = async (id, data) => {
@@ -36,6 +42,10 @@ export const updateOpenCriminalCase = async (id, data) => {
 			method: "PATCH",
 			body: JSON.stringify(data),
 			headers: { "Content-type": "application/json; charset=UTF-8" }
+		}).then(response => {
+			console.log(response)
+		}).catch(err => {
+			console.log(err)
 		})
 }
 
@@ -43,6 +53,10 @@ export const deleteOpenCriminalCase = async (id) => {
 	fetch(`${baseUrl}/${id}`,
 		{
 			method: "DELETE",
+		}).then(response => {
+			console.log(response)
+		}).catch(err => {
+			console.log(err)
 		})
 }
 
