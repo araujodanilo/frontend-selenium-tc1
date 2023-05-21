682c229feffeacf2d95c9923633fff1a098c3168
isarnf <isarnf@gmail.com>
2023-05-21 15:21:42 -0300

    fix(header): changed 'class' to 'className' in tags

--- src/layouts/HeaderLayout.jsx
+++ src/layouts/HeaderLayout.jsx
@@ -1,18 +1,18 @@
 const HeaderLayout = () => {
 	return (
 		<header className="p-3 mb-2 bg-dark text-white font-link">
-		<nav class="navbar navbar-expand-lg navbar-light bg-dark">
-			<div class="container-fluid">
+		<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+			<div className="container-fluid">
 				<a className="navbar-brand text-white" href="/">
 					<img src="/imgs/searcher.png" alt="" /> CSI SYSTEM
 				</a>
-				<div class="collapse navbar-collapse justify-content-end me-5" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item m-3">
-					<a class="nav-link" href="/crimes">Registros atuais</a>
+				<div className="collapse navbar-collapse justify-content-end me-5" id="navbarNav">
+				<ul className="navbar-nav">
+					<li className="nav-item m-3">
+					<a className="nav-link" href="/crimes">Registros atuais</a>
 					</li>
-					<li class="nav-item m-3">
-					<a class="nav-link" href="/crimes/register">Cadastrar</a>
+					<li className="nav-item m-3">
+					<a className="nav-link" href="/crimes/register">Cadastrar</a>
 					</li>
 				</ul>
 				</div>
