2780fd99674c7150d1dfb539bfd3e245889fcc8f
isarnf <isarnf@gmail.com>
2023-05-21 14:23:09 -0300

    style(header): moved nav-links to the right, changed color when links are on focus

--- src/layouts/HeaderLayout.jsx
+++ src/layouts/HeaderLayout.jsx
@@ -1,14 +1,31 @@
 const HeaderLayout = () => {
 	return (
-		<header className="p-3 mb-2 bg-dark text-white">
-			<nav className="navbar bg-dark navbar-expand-lg ms-auto font-link"  >
+		<header className="p-3 mb-2 bg-dark text-white font-link">
+		<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+			<div class="container-fluid">
 				<a className="navbar-brand text-white" href="/">
 					<img src="/imgs/searcher.png" alt="" /> CSI SYSTEM
 				</a>
-				<a className="nav-link text-white ms-5 me-5" href="/crimes">Registros atuais</a>
-				<a className="nav-link text-white" href="/crimes/register">Cadastrar</a>
+				<div class="collapse navbar-collapse justify-content-end me-5" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item m-3">
+					<a class="nav-link" href="/crimes">Registros atuais</a>
+					</li>
+					<li class="nav-item m-3">
+					<a class="nav-link" href="/crimes/register">Cadastrar</a>
+					</li>
+				</ul>
+				</div>
+			</div>
 			</nav>
-		</header>
+			</header>
+
+
+
+
+		
+
+		
 	)
 }
 
