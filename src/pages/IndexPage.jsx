a111c450d581739e7cb88da09cd8265da32a33cf
isarnf <isarnf@gmail.com>
2023-05-21 15:23:36 -0300

    fix(images): changed description of images in alt tag

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -9,13 +9,13 @@ const IndexPage = () => {
 				<p className="m-4 fs-5 text-center">CSI SYSTEM é um sistema FICTÍCIO para gerenciamento de registros criminais que tem como finalidade auxiliar policiais e outros profissionais da área de segurança a pesquisarem por informações relevantes sobre determinados crimes inventados. Esse site foi desenvolvido como exercício para a disciplina de Tópicos em Computação I do Instituto Federal de São Paulo para a prática de testes de software.</p>
 				<div className="d-flex">
 				<div className="container-faded d-flex align-items-center justify-content-center">
-					<img src="/imgs/board.jpg" alt="Avatar" className="image img-thumbnail border-dark" />
+					<img src="/imgs/board.jpg" alt="Foto de braço de pessoa apontando para quadro com informações de um crime." className="image img-thumbnail border-dark" />
 					<div className="middle">
 						<div className="text"><a href='/crimes'>Visualizar registros</a></div>
 					</div>
 				</div>
 				<div className="container-faded d-flex align-items-center justify-content-center">
-					<img src="/imgs/typing.jpg" alt="Avatar" className="image img-thumbnail border-dark" />
+					<img src="/imgs/typing.jpg" alt="Foto de braço de pessoa digitando em notebook." className="image img-thumbnail border-dark" />
 					<div className="middle">
 						<div className="text"><a href='/crimes/register'>Cadastrar</a></div>
 					</div>
