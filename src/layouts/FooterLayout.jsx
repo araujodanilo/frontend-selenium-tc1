1eb589fbeb65e09f981d23da72df1a722a007318
isarnf <isarnf@gmail.com>
2023-05-21 13:55:53 -0300

    feat(footer): added text to footer

--- src/layouts/FooterLayout.jsx
+++ src/layouts/FooterLayout.jsx
@@ -1,7 +1,13 @@
 const FooterLayout = () => {
 	return(
-		<footer className="p-3 mb-2 bg-dark text-white">
-			<p>FOOTER</p>
+		<footer className="p-3 mb-2 bg-dark text-white text-center">
+			<a className="navbar-brand text-white" href="/">
+					<img src="/imgs/searcher.png" alt="" /> CSI SYSTEM
+			</a>
+			<br />
+			<br />
+			<p>Contato: csisystem@email.com.br</p>
+			<p>Desenvolvido por: Danilo Araújo, Cauê Gastaldi, Isabela Forti</p>
 		</footer>
 	)
 }
